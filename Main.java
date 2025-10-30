2565c30a836b26de337827b71ed58eb703edf236
CarlosZenilLlanos <zeernesto1.2@gmail.com>
2025-10-29 18:57:53 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
-public class Main{
-  
+import java.util.scanner
+
+public class Main {
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+    String[] areglo = new String[20];
+    Posicion pos = new Posicion();
+    HiloEscribe hilo = new HiloEscribe(pos); //Creción del hilo
+
+    //Configuración del hilo
+    hilo.setInfo(arreglo);
+    hilo.setIni(0);
+    hilo.setInfo(1000);
+
+    int opcion = 0;
+    boolean hiloCreado = false;
+}
 }
